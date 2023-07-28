c407a3705dff180f5788aacf74523aa464c54a6c
Alfie Martin <alfiemartin01@gmail.com>
2023-07-28 14:36:12 +0100

    Added tests for books.py

---
+++ Python/tests/test_books.py
@@ -0,0 +1,38 @@
+from apps import books
+
+def test_books():
+
+    #Initial variable tests
+    assert books.books == [] 
+    
+    #__init__ tests
+
+    #adding a single object tests
+    b1 = books.Book('ILM',12,12,'Romance',"Athena")
+
+    assert b1.title == 'ILM'
+    assert b1.pages == 12
+    assert b1.isbn == 12
+    assert b1.genre == 'Romance'
+    assert b1.author == 'Athena'
+    assert books.books == [b1]
+
+    #adding new object with different isbn and no author input given
+    b2 = books.Book('ILW',14,14,'War') 
+    assert books.books == [b1,b2]
+    assert 
+    
+    #adding new object with same isbn as b1
+    b3 = books.Book('ILW',13,12,'Romcom',"Athens")
+    
+    assert b1.title == 'ILW'
+    assert b1.pages == '13'
+    assert b1.isbn == '12'
+    assert b1.genre == 'Romcom'
+    assert b1.author == 'Athens'
+    assert books.books == [b1,b2]
+
+    #__str__ tests 
+
+    assert b1.__str__ == 'title: ILW, Author: Athens, Genre: Romcom, Pages:13'
+
